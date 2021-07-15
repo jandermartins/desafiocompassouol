6be6e88594e20b2b4d88ee8f10be2f7fad2111a8
Jander Martins <jandermartins@alu.ufc.br>
2021-07-14 21:43:41 -0300

    subindo projeto springboot

---
+++ src/main/java/br/uol/compasso/desafio/desafiocompassouol/DesafiocompassouolApplication.java
@@ -0,0 +1,13 @@
+package br.uol.compasso.desafio.desafiocompassouol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocompassouolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocompassouolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uol/compasso/desafio/desafiocompassouol/DesafiocompassouolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uol.compasso.desafio.desafiocompassouol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocompassouolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
